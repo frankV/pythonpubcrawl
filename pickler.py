533ac53280bd9831360da65d57ea2d65a3aecbe9
frankV <fvalcarcel@me.com>
2013-03-12 14:59:47 -0400

    add pickler./py, will use later

---
+++ pickler.py
@@ -0,0 +1,49 @@
+import os, collections
+import cPickle as pickle
+
+
+# ---------------------------------------------------------------------------- #
+#   function - pickleDump
+#   saves "files" and "extensions" dict to a file
+# ---------------------------------------------------------------------------- #
+def pickleDump():
+  global files, extensions
+
+  print 'pickling...'
+  pickle.dump( files, open( "filesdict.p", "wb" ) )
+  pickle.dump( extensions, open( "extensionsdict.p", "wb" ) )
+
+
+# ---------------------------------------------------------------------------- #
+#   function - pickleLoad
+#   loads "files" and "extensions" dict from a file
+# ---------------------------------------------------------------------------- #
+def pickleLoad():
+  files = {}
+extensions = collections.defaultdict(int)
+  cwd = os.getcwd()
+
+  fileDictPickle = str(cwd) + '/filesdict.p'
+  extDictPickle = str(cwd) + '/extensionsdict.p'
+
+  print 'Loading files...'
+  files = pickle.load( open( fileDictPickle, "rb" ) )
+  print 'Loading extensions...'
+  extensions = pickle.load( open( extDictPickle, "rb" ) )
+
+
+# ---------------------------------------------------------------------------- #
+#   function - pickleTry
+#   checks to see if pickled files already exist
+# ---------------------------------------------------------------------------- #
+def pickleTry():
+  cwd = os.getcwd()
+
+  try:
+    with open(cwd+'/filesdict.p') as f:
+        pass
+        print 'pickle found'
+        return True
+  except IOError as e:
+    print 'pickle not found'
+    return False
