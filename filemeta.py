29884aa330b82b1e1b3b6e06f1c45c3b88aa12ba
frankV <fvalcarcel@me.com>
2013-05-15 16:55:21 -0400

    python is fucking awesome! oop transition complete

--- filemeta.py
+++ filemeta.py
@@ -5,43 +5,45 @@
 
 """ filemeta.py -- base class for file meta data  """
 
-import os, time, sys, stat
+import os, time, sys
+from stat import *
 
 class FileMeta(object):
 	"""docstring for FileMeta"""
 
 	def __init__(self, fullPathFileName, filename):
-		# super(FileMeta, self).__init__()
 
 		self.fullPathFileName = fullPathFileName
 		self.filename = filename
+		self.extension = self.getExtension()
+		self.created = self.getMetaData()[0]
+		self.modified = self.getMetaData()[1]
+		self.size = self.getMetaData()[2]
+		self.owner = self.getMetaData()[3]
+		self.permissions = self.getMetaData()[4]
 	
 	def getExtension(self):
 		return os.path.splitext(self.filename)[1].lower()
 
 	def getMetaData(self):
-		try: # file stat
+		try:
 			st = os.stat(self.fullPathFileName)
-	    except OSError, e:
-	        print "failed to get file info"
-	    else:
-          	# get file size and created date
-          	created = time.ctime(os.path.getctime(fullPathFileName))
-          	modified = time.ctime(os.path.getmtime(fullPathFileName))
+		except OSError, e:
+			print "failed to get file info"
+		else:
+			created = time.ctime(os.path.getctime(self.fullPathFileName))
+          	modified = time.ctime(os.path.getmtime(self.fullPathFileName))
           	size = st[ST_SIZE]
           	owner = st[ST_UID]
           	permissions = oct(st[ST_MODE])[-3:]
+         	return [created, modified, size, owner, permissions]
 
 	def fileMetaList(self):
-		return [self.filename, self.ext, self.created, self.modified, self.size, self.owner, self.permissions]
+		return [self.filename, self.extension, self.created, self.modified,\
+		        self.size, self.owner, self.permissions]
 
 
 
 
 
-	 # 	  self.ext = os.path.splitext(filename)[1].lower()
-	 # 	  self.created = time.ctime(os.path.getctime(fullPathFileName))
-	 #    self.modified = time.ctime(os.path.getmtime(fullPathFileName))
-	 #    self.size = st[ST_SIZE]
-	 #    self.owner = st[ST_UID]
-	 #    self.permissions = oct(st[ST_MODE])[-3:]
+
